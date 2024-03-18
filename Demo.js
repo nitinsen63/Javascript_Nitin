d4759e1570106148ff181a09425a426f2dd10fbe
nitinsen63 <64092119+nitinsen63@users.noreply.github.com>
2024-03-18 09:07:38 +0000

    NewChanges

--- Demo.js
+++ Demo.js
@@ -1,3 +1,8 @@
 console.log(typeof null);
 const MyMethod = new String('Mukesh')
 console.log(MyMethod.indexOf('h'));
+const Myname = new String('    Given      ')
+console.log(Myname.toUpperCase);
+const newNamefield = Myname.trim();
+console.log(newNamefield.length);
+console.log(Myname.length);
