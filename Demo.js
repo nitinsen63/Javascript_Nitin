b085fc58e6a3e7735fdba5e24c6cbaaecdb2ce38
nitinsen63 <64092119+nitinsen63@users.noreply.github.com>
2024-03-18 09:40:39 +0000

    ChangesHere

--- Demo.js
+++ Demo.js
@@ -6,3 +6,8 @@ console.log(Myname.toUpperCase);
 const newNamefield = Myname.trim();
 console.log(newNamefield.length);
 console.log(Myname.length);
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+const min =20;
+const max =30;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
